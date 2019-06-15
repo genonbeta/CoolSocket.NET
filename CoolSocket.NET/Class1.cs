514a65c089f2f71b90703fcc5904573b853e55f4
Veli <veli.tasali@gmail.com>
2019-06-15 23:20:59 +0300

    Basic structure for CoolSocket
    The name CoolSocket was previously used by a library named CoolSocket. It may be a problem.

--- CoolSocket.NET/Class1.cs
+++ CoolSocket.NET/Class1.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 
 namespace CoolSocket.NET
 {
-    public class CoolSocket
+    abstract public class Server
     {
+        public const string HeaderSeperator = "\nHEADER_END\n";
+        public const string HeaderLength = "length";
+
+        protected internal List<string> ActiveConnections { get; set; }
+        protected internal TcpListener TcpServer { get; set; }
+
+        public Server() : this(0)
+        {
+            
+        }
+
+        public Server(Int16 port) : this (IPAddress.Parse("0.0.0.0"), port)
+        {
+            
+        }
+
+        public Server(IPAddress ipAddress, short port)
+        {
+            this.TcpServer = new TcpListener(ipAddress, port);
+        }
     }
 }
